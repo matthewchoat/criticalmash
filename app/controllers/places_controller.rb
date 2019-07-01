78d3618966fb3d199d5f43b4c4acdc44483d0643
matthewchoat <matthewchoat@gmail.com>
2019-07-01 03:57:53 +0000

    nomster25

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
 
   include Pagy::Backend
 
@@ -23,10 +23,18 @@ class PlacesController < ApplicationController
 
   def edit
     @place = Place.find(params[:id])
+
+      if @place.user != current_user
+        return render plain: 'Not Allowed', status: :forbidden
+      end
   end
 
   def update
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
+
     @place.update_attributes(place_params)
     redirect_to root_path
   end
