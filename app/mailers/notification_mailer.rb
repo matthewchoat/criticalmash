e9e88201b3161dcf677392c87ab87f1d6b724d51
matthewchoat <matthewchoat@gmail.com>
2019-07-11 04:08:58 +0000

    lesson 41

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
   def comment_added
-    mail(to: "matthewchoat@gmail.com",
+    mail(to: "anorcostudios@gmail.com",
         subject: "A comment has been added to your place")
   end
 end
