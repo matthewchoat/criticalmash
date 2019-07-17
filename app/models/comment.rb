1aa93fc271dcaf80d203576cd5991961027502c7
matthewchoat <matthewchoat@gmail.com>
2019-07-17 03:09:17 +0000

    bug fixes pt2

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,11 +4,11 @@ class Comment < ApplicationRecord
   after_create :send_comment_email
 
   RATINGS = {
-    'one star': '1_star',
-    'two stars': '2_stars',
-    'three stars': '3_stars',
-    'four stars': '4_stars',
-    'five stars': '5_stars'
+    '☻☺☺☺☺': '1_star',
+    '☻☻☺☺☺': '2_stars',
+    '☻☻☻☺☺': '3_stars',
+    '☻☻☻☻☺': '4_stars',
+    '☻☻☻☻☻': '5_stars'
   }
 
   def humanized_rating
